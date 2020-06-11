d09d3cd3917057df9509e3488bada8bfcb3f9dd5
Ilmir2018 <ilmir2007@inbox.ru>
2020-06-11 16:56:44 +0300

    add template and loading data from the database

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Page;
+use App\Models\Portfolio;
+use App\Models\Service;
+use App\Models\People;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+
+    public function execute(Request $request) {
+
+        $pages = Page::all();
+        $portfolios = Portfolio::get(['name', 'filter', 'images']);
+        $services = Service::all();
+        $peoples = People::take(3)->get();
+
+        $tags = DB::table('portfolios')->distinct()->get(['filter']);
+
+        $menu = [];
+        foreach ($pages as $page) {
+            $item = ['title' => $page->name, 'alias' => $page->alias];
+            array_push($menu, $item);
+        }
+
+        $item = ['title' => 'Services', 'alias' => 'service'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Portfolio', 'alias' => 'Portfolio'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Team', 'alias' => 'team'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Contact', 'alias' => 'contact'];
+        array_push($menu, $item);
+
+        return view('site.index', [
+            'menu' => $menu,
+            'pages' => $pages,
+            'services' => $services,
+            'portfolios' => $portfolios,
+            'peoples' => $peoples,
+            'tags' => $tags
+            ]);
+    }
+}
---
+++ database/migrations/2020_06_05_081139_create_table_portfolios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePortfolios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 200);
+            $table->string('images', 100);
+            $table->string('filter',100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
